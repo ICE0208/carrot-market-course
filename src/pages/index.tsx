babbbc99a2c3ce2d4a856d96b8bd37cd3f183b59
ICE0208 <doky3307@naver.com>
2023-10-11 00:23:46 +0900

    Setup prettier-plugin-tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="bg-red-300">
-      <h1 className="text-blue-600">It works</h1>
+      <h1 className="p-3 pt-2 text-blue-600">It works</h1>
     </div>
   );
 }
