92d1dc4f9624381a82c7d8d80f4f7d2370ebb504
ICE0208 <doky3307@naver.com>
2023-10-11 01:19:33 +0900

    (#4.1) Test Drive part One

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <div className="bg-red-300">
-      <h1 className="p-3 pt-2 text-blue-600">It works</h1>
+    <div className="flex flex-col space-y-10 bg-slate-400 px-10 py-20">
+      <div className="rounded-2xl bg-white p-6 shadow-xl">
+        <span className="text-3xl font-semibold">Select Item</span>
+        <div className="my-2 flex justify-between">
+          <span className="text-grey-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-grey-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
+          <span>Total</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="m-auto mt-5 w-3/5 rounded-xl bg-blue-500 p-3 text-center text-white">
+          Checkout
+        </div>
+      </div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
     </div>
   );
 }
