182915dbb6727cc1a8b0baa1d3627f94556415e8
ICE0208 <doky3307@naver.com>
2023-10-18 23:28:31 +0900

    (#6.0) Before We Start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,42 @@
-export default function Home() {
+import type { NextPage } from "next";
+import FloatingButton from "../components/floating-button";
+import Item from "../components/item";
+import Layout from "../components/layout";
+
+const Home: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-10 bg-slate-400 px-10 py-20">
-      <div className="rounded-2xl bg-white p-6 shadow-xl">
-        <span className="text-3xl font-semibold">Select Item</span>
-        <div className="my-2 flex justify-between">
-          <span className="text-grey-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-grey-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
-          <span>Total</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="m-auto mt-5 w-3/5 rounded-xl bg-blue-500 p-3 text-center text-white">
-          Checkout
-        </div>
+    <Layout title="홈" hasTabBar>
+      <div className="flex flex-col space-y-5 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            id={i}
+            key={i}
+            title="iPhone 14"
+            price={99}
+            comments={1}
+            hearts={1}
+          />
+        ))}
+        <FloatingButton href="/items/upload">
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </FloatingButton>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-    </div>
+    </Layout>
   );
-}
+};
+
+export default Home;
