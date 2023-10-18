43491722da32ad46845b1e4e0545ac4df1e47656
ICE0208 <doky3307@naver.com>
2023-10-19 02:27:41 +0900

    (#7.2) The Register Function

---
+++ src/pages/forms.tsx
@@ -0,0 +1,15 @@
+import { useForm } from "react-hook-form";
+
+export default function Forms() {
+  const { register, watch } = useForm();
+  console.log(watch());
+
+  return (
+    <form>
+      <input {...register("username")} type="text" placeholder="Username" />
+      <input {...register("email")} type="email" placeholder="Email" />
+      <input {...register("password")} type="password" placeholder="Password" />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
