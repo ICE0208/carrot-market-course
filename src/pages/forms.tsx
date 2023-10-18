69e96c1fe82c27c2d62d9d8fec98eafed1101281
ICE0208 <doky3307@naver.com>
2023-10-19 02:37:49 +0900

    (#7.4) Validation

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,14 +1,42 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit } = useForm<LoginForm>();
+
+  const onValid = (data: LoginForm) => {
+    console.log("valid");
+  };
+
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
 
   return (
-    <form>
-      <input {...register("username")} type="text" placeholder="Username" />
-      <input {...register("email")} type="email" placeholder="Email" />
-      <input {...register("password")} type="password" placeholder="Password" />
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", { required: "Username is required" })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", { required: "Email is required" })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", {
+          required: "Password is required",
+          minLength: { value: 5, message: "min length is 5" },
+        })}
+        type="password"
+        placeholder="Password"
+      />
       <input type="submit" value="Create Account" />
     </form>
   );
